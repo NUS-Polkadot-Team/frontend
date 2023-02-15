2d0e5cc0419c37841f7f83dd698651926fd1913f
Marcus Pang <marcuspangyuyang@gmail.com>
2023-02-16 06:47:58 +0800

    feat: update context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,30 +2,20 @@ import '@/styles/tailwind.css';
 import 'focus-visible';
 
 import { Layout } from '@/components/Layout';
-import {
-  SubstrateContextProvider,
-  useSubstrate,
-} from '@/context/SubstrateContext';
 import type { AppProps } from 'next/app';
-import { useEffect, useState } from 'react';
 
 const layoutRoutes = ['/bounty'];
 
 export default function App({ Component, pageProps, router }: AppProps) {
   const showLayout = layoutRoutes.includes(router.route);
-  // const { apiState, keyring, keyringState, apiError } = useSubstrate();
 
-  // console.log({ apiState, keyring, keyringState, apiError });
-
-  return (
-    <SubstrateContextProvider>
-      {showLayout ? (
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      ) : (
+  if (showLayout) {
+    return (
+      <Layout>
         <Component {...pageProps} />
-      )}
-    </SubstrateContextProvider>
-  );
+      </Layout>
+    );
+  }
+
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,86 @@
-import Head from 'next/head'
-
-import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
-import { Hero } from '@/components/Hero'
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+import { Hero } from '@/components/Hero';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { ContractPromise } from '@polkadot/api-contract';
+import type { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import abi from '@/context/metadata';
 
 export default function Home() {
+  const [blockchainUrl, setBlockchainUrl] = useState('ws://127.0.0.1:9944');
+  const [api, setApi] = useState<any>();
+  const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
+  const [block, setBlock] = useState(0);
+  const [lastBlockHash, setLastBlockHash] = useState('');
+  const [contractAddress, setContractAddress] = useState('');
+  const [actingAddress, setActingAddress] = useState('');
+  const [result, setResult] = useState('');
+  const [gasConsumed, setGasConsumed] = useState<any>();
+  const [outcome, setOutcome] = useState('');
+
+  const extensionSetup = async () => {
+    const { web3Accounts, web3Enable } = await import(
+      '@polkadot/extension-dapp'
+    );
+    const extensions = await web3Enable('Polk4NET');
+    if (extensions.length === 0) {
+      return;
+    }
+    const account = await web3Accounts();
+    setAccounts(account);
+  };
+
+  const setup = async () => {
+    const wsProvider = new WsProvider(blockchainUrl);
+    const api = await ApiPromise.create({ provider: wsProvider });
+    await api.rpc.chain.subscribeNewHeads((lastHeader) => {
+      setBlock(lastHeader.number.toNumber());
+      setLastBlockHash(lastHeader.hash.toString());
+    });
+    setApi(api);
+    await extensionSetup();
+  };
+
+  const getFlipValue = async () => {
+    const contract = new ContractPromise(api, abi, contractAddress);
+    const { gasConsumed, result, output } = await contract.query.get(
+      actingAddress,
+      { value: 0, gasLimit: -1 }
+    );
+    setGasConsumed(gasConsumed.toHuman());
+    setResult(JSON.stringify(result.toHuman()));
+    if (output !== undefined && output !== null) {
+      setOutcome(output.toHuman()?.toString() ?? '');
+    }
+  };
+
+  const changeFlipValue = async () => {
+    const { web3FromSource } = await import('@polkadot/extension-dapp');
+    const contract = new ContractPromise(api, abi, contractAddress);
+    const performingAccount = accounts[0];
+    const injector = await web3FromSource(performingAccount.meta.source);
+    const flip = await contract.tx.flip({ value: 0, gasLimit: -1 });
+    if (injector !== undefined) {
+      flip.signAndSend(
+        performingAccount.address,
+        { signer: injector.signer },
+        (result) => {
+          if (result.status.isInBlock) {
+            setResult('in a block');
+          } else if (result.status.isFinalized) {
+            setResult('finalized');
+          }
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    setup();
+  });
+
   return (
     <>
       <Head>
@@ -20,5 +96,5 @@ export default function Home() {
       </main>
       <Footer />
     </>
-  )
+  );
 }
