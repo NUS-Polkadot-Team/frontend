353ef4d311dc528ca1574dc82d24735f9658bf4e
Marcus Pang <marcuspangyuyang@gmail.com>
2023-02-18 01:36:55 +0800

    feat: improve button sizes for landing page

---
+++ src/components/CreateBountyPage/CreateForm.tsx
@@ -0,0 +1,133 @@
+import useCreateBounty from '@/hooks/useCreateBounty';
+import { useState, type ChangeEvent, type FormEvent } from 'react';
+import { toast } from 'react-toastify';
+import { FormData, customInputStyles } from '../../pages/create';
+
+const SHIBUYA_DECIMALS = 18;
+export function CreateForm() {
+  const [formData, setFormData] = useState<FormData>({
+    title: '',
+    image: undefined,
+    description: '',
+    prize: 0,
+    deadline: '',
+  });
+  const { createBounty, data, error } = useCreateBounty();
+
+  const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await createBounty(
+      'Qmde2yQH1VNuDXvAdPFnZnZABSEZwmbSmxkNoBwtazDJUd',
+      // TODO: fix unsafe number conversion
+      formData.prize * 10 ** SHIBUYA_DECIMALS
+    );
+    if (error) {
+      toast.error("Couldn't create bounty, please check if you are logged in.");
+      return;
+    }
+    toast.success(
+      <div>
+        <h3>Successfully created bounty</h3>
+        <span>Address: {data?.toString()}</span>
+      </div>
+    );
+  };
+
+  const handleInputChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleFileInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    setFormData((prevState) => ({
+      ...prevState,
+      image: file,
+    }));
+  };
+
+  return (
+    <form
+      onSubmit={handleFormSubmit}
+      className="flex max-w-7xl flex-col space-y-6 px-6 py-6"
+    >
+      <div>
+        <label htmlFor="title" className={customInputStyles.inputLabel}>
+          Title:
+        </label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={formData.title}
+          onChange={handleInputChange}
+          required
+          className={customInputStyles.inputField}
+        />
+      </div>
+      <div>
+        <label htmlFor="image" className={customInputStyles.inputLabel}>
+          Image:
+        </label>
+        <input
+          type="file"
+          id="image"
+          name="image"
+          accept="image/*"
+          onChange={handleFileInputChange}
+          className={customInputStyles.inputField}
+        />
+      </div>
+      <div>
+        <label htmlFor="description" className={customInputStyles.inputLabel}>
+          Description:
+        </label>
+        <textarea
+          id="description"
+          name="description"
+          value={formData.description}
+          onChange={handleInputChange}
+          required
+          className={customInputStyles.inputField}
+        />
+      </div>
+      <div>
+        <label htmlFor="prize" className={customInputStyles.inputLabel}>
+          Prize:
+        </label>
+        <input
+          type="number"
+          id="prize"
+          name="prize"
+          value={formData.prize}
+          onChange={handleInputChange}
+          required
+          className={customInputStyles.inputField}
+        />
+      </div>
+      <div>
+        <label htmlFor="deadline" className={customInputStyles.inputLabel}>
+          Deadline:
+        </label>
+        <input
+          type="date"
+          id="deadline"
+          name="deadline"
+          value={formData.deadline}
+          onChange={handleInputChange}
+          required
+          className={customInputStyles.inputField}
+        />
+      </div>
+      <button type="submit" className={customInputStyles.submitButton}>
+        Submit
+      </button>
+    </form>
+  );
+}
--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,11 +1,8 @@
 import { Container } from '@/components/Layout/LayoutContainer';
-import useCreateBounty from '@/hooks/useCreateBounty';
 import Head from 'next/head';
-import { useState, type ChangeEvent, type FormEvent } from 'react';
-import { toast } from 'react-toastify';
-import BN from 'bn.js';
+import { CreateForm } from '../components/CreateBountyPage/CreateForm';
 
-type FormData = {
+export type FormData = {
   title: string;
   image?: File;
   description: string;
@@ -21,142 +18,11 @@ export const customInputStyles = {
     'inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
 };
 
-const SHIBUYA_DECIMALS = 18;
-
-function CreateForm() {
-  const [formData, setFormData] = useState<FormData>({
-    title: '',
-    image: undefined,
-    description: '',
-    prize: 0,
-    deadline: '',
-  });
-  const { createBounty, data, error } = useCreateBounty();
-
-  const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    await createBounty(
-      'Qmde2yQH1VNuDXvAdPFnZnZABSEZwmbSmxkNoBwtazDJUd',
-      // TODO: fix unsafe number conversion
-      formData.prize * 10 ** 18
-    );
-    if (error) {
-      toast.error("Couldn't create bounty, please check if you are logged in.");
-      return;
-    }
-    toast.success(
-      <div>
-        <h3>Successfully created bounty</h3>
-        <span>Address: {data?.toString()}</span>
-      </div>
-    );
-  };
-  // toast.success('hello');
-
-  const handleInputChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = event.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleFileInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    setFormData((prevState) => ({
-      ...prevState,
-      image: file,
-    }));
-  };
-
-  return (
-    <form
-      onSubmit={handleFormSubmit}
-      className="flex max-w-7xl flex-col space-y-6 px-6 py-6"
-    >
-      <div>
-        <label htmlFor="title" className={customInputStyles.inputLabel}>
-          Title:
-        </label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          value={formData.title}
-          onChange={handleInputChange}
-          required
-          className={customInputStyles.inputField}
-        />
-      </div>
-      <div>
-        <label htmlFor="image" className={customInputStyles.inputLabel}>
-          Image:
-        </label>
-        <input
-          type="file"
-          id="image"
-          name="image"
-          accept="image/*"
-          onChange={handleFileInputChange}
-          className={customInputStyles.inputField}
-        />
-      </div>
-      <div>
-        <label htmlFor="description" className={customInputStyles.inputLabel}>
-          Description:
-        </label>
-        <textarea
-          id="description"
-          name="description"
-          value={formData.description}
-          onChange={handleInputChange}
-          required
-          className={customInputStyles.inputField}
-        />
-      </div>
-      <div>
-        <label htmlFor="prize" className={customInputStyles.inputLabel}>
-          Prize:
-        </label>
-        <input
-          type="number"
-          id="prize"
-          name="prize"
-          value={formData.prize}
-          onChange={handleInputChange}
-          required
-          className={customInputStyles.inputField}
-        />
-      </div>
-      <div>
-        <label htmlFor="deadline" className={customInputStyles.inputLabel}>
-          Deadline:
-        </label>
-        <input
-          type="date"
-          id="deadline"
-          name="deadline"
-          value={formData.deadline}
-          onChange={handleInputChange}
-          required
-          className={customInputStyles.inputField}
-        />
-      </div>
-      <button type="submit" className={customInputStyles.submitButton}>
-        Submit
-      </button>
-    </form>
-  );
-}
-
 export default function CreatePage() {
   return (
     <>
       <Head>
-        <title>BountiFi - Create</title>
+        <title>BountiFi - Create Bounty</title>
         <meta
           name="description"
           content="Building communities of artists, Empowering the creator economy"
